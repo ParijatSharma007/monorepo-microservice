a898d6cbd3ab5bf3e81958ea8eee771cc7e76d24
anil-arnxt <anil.samal@arnxt.com>
2025-06-08 21:12:17 +0530

    server added

---
+++ apps/server/src/index.ts
@@ -0,0 +1,22 @@
+import Fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
+
+const fastify = Fastify({ logger: true })
+const prisma = new PrismaClient()
+
+fastify.get('/', async (request, reply) => {
+  const users = await prisma.user.findMany()
+  return users
+})
+
+const start = async () => {
+  try {
+    await fastify.listen({ port: 3001 })
+    console.log('Server running at http://localhost:3001')
+  } catch (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+}
+
+start()
